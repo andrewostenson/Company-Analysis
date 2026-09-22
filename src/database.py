52c52b5ed82454fd71885d02dcce5a2c0593509e
andrewostenson <ostensonandrew@gmail.com>
2026-09-22 17:32:25 -0600

    Added check for already existing db

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ import embedding as em
 import sec_ret as sec
 
 chroma_client = chromadb.PersistentClient(path="/home/ubuntu/Company-Analysis/chroma_data")
-collection = chroma_client.create_collection(name="secAnalysis")
+collection = chroma_client.get_or_create_collection(name="secAnalysis")
 
 def buildCollection():
     chunks = sec.getChunks()
@@ -14,3 +14,6 @@ def buildCollection():
     doc_ids = [str(x) for x in range(len(documents))]
 
     collection.add(ids = doc_ids, embeddings = embeddings, metadatas = metadata, documents = documents)
+
+if __name__ == "__main__":
+    buildCollection()
