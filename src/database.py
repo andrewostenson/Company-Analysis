d4a1aaecafeffec64578849ed3d4db6545e80775
andrewostenson <ostensonandrew@gmail.com>
2026-09-23 10:35:35 -0600

    Added embedding retrieval for user input

--- src/database.py
+++ src/database.py
@@ -6,14 +6,24 @@ chroma_client = chromadb.PersistentClient(path="/home/ubuntu/Company-Analysis/ch
 collection = chroma_client.get_or_create_collection(name="secAnalysis")
 
 def buildCollection():
+    model = em.intModel()
     chunks = sec.getChunks()
 
     metadata = sec.getMetadata(chunks)
-    documents = sec.getPageContent(chunks)
+    documents = sec.getPageContent(chunks, model)
     embeddings = em.embedPageContent(documents)
     doc_ids = [str(x) for x in range(len(documents))]
 
     collection.add(ids = doc_ids, embeddings = embeddings, metadatas = metadata, documents = documents)
 
+def queryCollection(embeddings):
+    results = collection.query(
+        query_embeddings = embeddings
+    )
+    print(results)
+    return results
+
 if __name__ == "__main__":
+    q = input("Enter query")
     buildCollection()
+    queryCollection(q)
--- src/embedding.py
+++ src/embedding.py
@@ -1,11 +1,18 @@
 import sec_ret as sec
 from sentence_transformers import SentenceTransformer
 
-def embedChunks(chunks):
+def intModel():
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return model;
+
+def embedChunks(chunks, model):
     embeddings = model.encode(chunks)
     return embeddings
 
-def embedPageContent(pageContent):
-    embeddings = embedChunks(pageContent)
+def embedPageContent(pageContent, model):
+    embeddings = embedChunks(pageContent, model)
+    return embeddings
+
+def embedUserInput(uInput, model):
+    embeddings = model.encode(uInput)
     return embeddings
